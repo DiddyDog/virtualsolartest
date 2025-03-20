df62bea30410267cc4d1b08826228659ad6729e9
YangChowRice <tth260303@gmail.com>
2025-03-20 21:05:59 +1100

    123
    123

---
+++ virtual solar UI/Views/JOhn.swift
@@ -0,0 +1,18 @@
+//
+//  JOhn.swift
+//  virtual solar UI
+//
+//  Created by Trong Huy on 20/3/2025.
+//
+
+import SwiftUI
+
+struct JOhn: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    JOhn()
+}
