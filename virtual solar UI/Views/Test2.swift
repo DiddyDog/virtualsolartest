d0767331466450e5c9a26b77380efd6ea1bd180d
DiddyDog <lachlanjiang@gmail.com>
2025-03-20 21:03:59 +1100

    test2 for john and ed

---
+++ virtual solar UI/Views/Test2.swift
@@ -0,0 +1,18 @@
+//
+//  Test2.swift
+//  virtual solar UI
+//
+//  Created by Lachlan Jiang on 20/3/2025.
+//
+
+import SwiftUI
+
+struct Test2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test2()
+}
