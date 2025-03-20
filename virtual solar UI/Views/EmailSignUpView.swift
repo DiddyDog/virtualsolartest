343779a206637d4161ab613c3c99cb857b1fab05
DiddyDog <lachlanjiang@gmail.com>
2025-03-20 16:33:36 +1100

    New Email View

---
+++ virtual solar UI/Views/EmailSignUpView.swift
@@ -0,0 +1,18 @@
+//
+//  EmailSignUpView.swift
+//  virtual solar UI
+//
+//  Created by Lachlan Jiang on 20/3/2025.
+//
+
+import SwiftUI
+
+struct EmailSignUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EmailSignUpView()
+}
